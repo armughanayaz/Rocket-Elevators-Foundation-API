b50c5a52ca0146279ee8c92f8e2ddac31ea4455d
lemyted <67526512+lemyted@users.noreply.github.com>
2021-11-23 18:00:18 -0500

    Added Lukabranch for endpoint#10

--- Controllers/BatteriesController.cs
+++ Controllers/BatteriesController.cs
@@ -1,3 +1,5 @@
+using System;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -5,23 +7,61 @@ using RocketApi.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 
+
 namespace RocketApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+
     public class BatteriesController : ControllerBase
     {
         private readonly ApplicationContext _context;
 
-        public BatteriesController(ApplicationContext context) 
+
+        public BatteriesController(ApplicationContext context)
         {
             _context = context;
         }
 
         [HttpGet]
-        public List<Battery>  GetBatteries()
-        {
-            return _context.batteries.ToList();
+        public async Task<dynamic> GetAllBatteries(){
+
+            var batteries = await _context.batteries.ToListAsync();
+
+            var i = 0;
+
+            var numbers = new List<Int64>(){};
+            foreach(Battery battery in batteries)
+            {
+                i++;
+            }
+            numbers.Add(i);
+
+            return numbers;
         }
-    }  
-}
+
+        [HttpGet("{id}/status")]
+        public async Task<ActionResult<string>> GetBatteryStatus(long id)
+        {
+            var battery = await _context.batteries.FindAsync(id);
+
+            if (battery == null)
+            {
+                return NotFound();
+            }
+
+            return battery.Status;
+        }
+
+        [HttpGet("update/{id}/{status}")]
+        public async Task<dynamic> test(string status, long id)
+        {
+            var battery = await _context.batteries.FindAsync(id);
+
+            battery.Status = status;
+            await _context.SaveChangesAsync();         
+
+            return battery;
+        }
+    }
+} 
